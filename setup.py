4f0c3aa033f98bb62e29ae205346e50e872e217c
hector <hector.alonso@osoigo.com>
2022-11-27 21:04:50 -0500

    fix app

--- setup.py
+++ setup.py
@@ -6,25 +6,22 @@ from setuptools import (
 )
 
 extras_require = {
-    "test": [
+    'test': [
         "hypothesis>=4.18.0,<5",
-        "pytest>=6.2.5,<7",
+        "pytest>=4.4.0,<5",
         "pytest-xdist",
-        "tox==3.25.0",
+        "tox>=2.9.1,<3",
     ],
-    "lint": [
-        "flake8==3.7.9",
+    'lint': [
+        "flake8==3.4.1",
         "isort>=4.2.15,<5",
-        "mypy==0.910",
-        "pydocstyle>=5.0.0,<6",
+        "pydocstyle>=3.0.0,<4",
     ],
-    "doc": [
-        "Sphinx>=1.6.5,<5",
-        "jinja2>=3.0.0,<3.1.0",  # jinja2<3.0 or >=3.1.0 cause doc build failures.
-        "sphinx_rtd_theme>=0.1.9,<1",
-        "towncrier>=21,<22",
+    'doc': [
+        "Sphinx>=1.6.5,<2",
+        "sphinx_rtd_theme>=0.1.9",
     ],
-    "dev": [
+    'dev': [
         "bumpversion>=0.5.3,<1",
         "pytest-watch>=4.1.0,<5",
         "wheel",
@@ -33,63 +30,48 @@ extras_require = {
     ],
 }
 
-extras_require["dev"] = (
-    extras_require["dev"]
-    + extras_require["test"]  # noqa: W504
-    + extras_require["lint"]  # noqa: W504
-    + extras_require["doc"]  # noqa: W504
+extras_require['dev'] = (
+    extras_require['dev'] +
+    extras_require['test'] +
+    extras_require['lint'] +
+    extras_require['doc']
 )
 
-
-with open("./README.md") as readme:
-    long_description = readme.read()
-
-
 setup(
-    name="eth-account",
+    name='eth-account',
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version="0.7.0",
+    version='0.4.0',
     description="""eth-account: Sign Ethereum transactions and messages with local private keys""",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="The Ethereum Foundation",
-    author_email="snakecharmers@ethereum.org",
-    url="https://github.com/ethereum/eth-account",
+    long_description_markdown_filename='README.md',
+    author='The Ethereum Foundation',
+    author_email='snakecharmers@ethereum.org',
+    url='https://github.com/ethereum/eth-account',
     include_package_data=True,
-    package_data={
-        "eth_account": [
-            "py.typed",
-            "hdaccount/wordlist/*.txt",
-        ]
-    },
     install_requires=[
-        "bitarray>=2.4.0,<3",
-        "eth-abi>=3.0.0,<4",
-        "eth-keyfile>=0.6.0,<0.7.0",
-        "eth-keys>=0.4.0,<0.5",
-        "eth-rlp>=0.3.0,<1",
-        "eth-utils>=2.0.0,<3",
+        "attrdict>=2.0.0,<3",
+        "eth-abi>=2.0.0b7,<3",
+        "eth-keyfile>=0.5.0,<0.6.0",
+        "eth-keys>=0.2.1,<0.3.0",
+        "eth-rlp>=0.1.2,<1",
+        "eth-utils>=1.3.0,<2",
         "hexbytes>=0.1.0,<1",
-        "rlp>=1.0.0,<4",
+        "rlp>=1.0.0,<2"
     ],
-    python_requires=">=3.6, <4",
+    setup_requires=['setuptools-markdown'],
+    python_requires='>=3.6, <4',
     extras_require=extras_require,
-    py_modules=["eth_account"],
+    py_modules=['eth_account'],
     license="MIT",
     zip_safe=False,
-    keywords="ethereum",
+    keywords='ethereum',
     packages=find_packages(exclude=["tests", "tests.*"]),
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Operating System :: MacOS",
-        "Operating System :: POSIX",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
